ae76b862361a3d4a312df4f5abaae2ef1b0f160b
mlblagerberg <mlblagerberg@gmail.com>
2024-05-28 16:41:14 -0700

    Starting to process features.

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 # -------------------------------------- IMPORT DATA ------------------------------------ #
 data = pd.read_csv("Data/train.csv")
@@ -20,7 +22,7 @@ data["PassengerId"] = data["PassengerId"].astype(object)
 data[["LastName", "SuffixFirstName"]] = data["Name"].str.split(",", expand=True)
 data[["Suffix", "FirstName"]] = data["SuffixFirstName"].str.split(".", n=1, expand=True)
 data.drop(columns=["Name", "SuffixFirstName"], inplace=True)
-# print(data.head())
+print(data.head())
 # print(data.columns)
 # print(data.dtypes)
 # print(data.describe())
@@ -32,6 +34,7 @@ data.drop(columns=["Name", "SuffixFirstName"], inplace=True)
 median_age = data["Age"].median()  # 28
 # print(median_age)
 
+# Correlation matrix
 # numerical_data = data.select_dtypes(include=["int", "float"])
 # correlation_matrix = numerical_data.corr()
 # plt.figure(figsize=(10, 8))
@@ -43,23 +46,38 @@ median_age = data["Age"].median()  # 28
 children_data = data[(data["Age"] < 16) & (data["Parch"] != 0)]
 # print(sum(children_data["Survived"])/len(children_data["Survived"]))
 # print(children_data.describe())
+
 # -------------------------------------- BASELINE MODEL ------------------------------------ #
-# Baseline only uses numerical features
-base_data = data.dropna()
-X = base_data[["Pclass", "Age", "SibSp", "Parch", "Fare"]].values
-y = base_data["Survived"].values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-# print("Shape of X_train:", X_train.shape)
-
-model = LogisticRegression()
-model.fit(X_train, y_train)
-
-# prediction
-y_pred = model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy}")  # Accuracy: 0.7027027027027027
+# # Baseline only uses numerical features
+# base_data = data.dropna()
+# X = base_data[["Pclass", "Age", "SibSp", "Parch", "Fare"]].values
+# y = base_data["Survived"].values
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# # print("Shape of X_train:", X_train.shape)
+#
+# model = LogisticRegression()
+# model.fit(X_train, y_train)
+#
+# # prediction
+# y_pred = model.predict(X_test)
+# accuracy = accuracy_score(y_test, y_pred)
+# print(f"Accuracy: {accuracy}")  # Accuracy: 0.7027027027027027
 
 # -------------------------------------- PREPROCESSING ------------------------------------ #
+columns_with_null = data.columns[data.isna().any()].tolist()
+# print(f"Columns with nulls: {columns_with_null}")  # Age, Cabin, Embarked
+# print(data[data["Cabin"].isna()].describe())
+data["Sex"] = data["Sex"].astype(bool)
+# print(data.dtypes)
+# print(data.head())
+# print(data["Ticket"].unique())
+# preprocessor = ColumnTransformer(
+#     transformers=[
+#         ('ice' OneHotEncoder(), ["Embarked"])
+#     ]
+#
+# )
+
 
 # -------------------------------------- IMPUTE NULL AGE ------------------------------------ #
 null_age = data[data["Age"].isna()]
