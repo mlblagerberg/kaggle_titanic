ae759b147e55d602c1b09354ea35449ef429861b
mlblagerberg <mlblagerberg@gmail.com>
2024-05-28 22:04:50 -0700

    Fitting additional basic models and output results.

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import pandas as pd
 import seaborn as sns
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
@@ -22,6 +24,8 @@ data["PassengerId"] = data["PassengerId"].astype(object)
 data[["LastName", "SuffixFirstName"]] = data["Name"].str.split(",", expand=True)
 data[["Suffix", "FirstName"]] = data["SuffixFirstName"].str.split(".", n=1, expand=True)
 data.drop(columns=["Name", "SuffixFirstName"], inplace=True)
+# cabin_suffix = data["Cabin"].str[0]
+# data["CabinSuffix"] = cabin_suffix
 # print(data.head())
 # print(data.columns)
 # print(data.dtypes)
@@ -33,8 +37,8 @@ data.drop(columns=["Name", "SuffixFirstName"], inplace=True)
 # course these two variables are also highly correlated. As are parch and sibsp
 median_age = data["Age"].median()  # 28
 median_age_by_suffix = data.groupby("Suffix")["Age"].median()
-print("Median age by suffix:")
-print(median_age_by_suffix)
+# print("Median age by suffix:")
+# print(median_age_by_suffix)
 # print(median_age)
 
 # Correlation matrix
@@ -53,7 +57,7 @@ print(median_age_by_suffix)
 # -------------------------------------- BASELINE MODEL ------------------------------------ #
 # # Baseline only uses numerical features
 base_data = data.dropna()
-print(base_data.head(50))
+# print(base_data.head(50))
 # miss_median_age = base_data.loc[data["Suffix"] == "Miss", "Age"].median(skipna=True)
 # print(miss_median_age)
 # features = ["Pclass", "Age", "SibSp", "Parch", "Fare"]
@@ -93,6 +97,7 @@ features = ["Pclass",
             "Parch",
             "Fare",
             "Sex",
+            # "CabinSuffix",
             # "Embarked",
             # "Ticket",
             # "Cabin",
@@ -101,7 +106,7 @@ features = ["Pclass",
 X = (base_data[features]) #.values
 y = base_data["Survived"] #.values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=44)
 
 preprocessor = ColumnTransformer(
     transformers=[
@@ -109,26 +114,45 @@ preprocessor = ColumnTransformer(
         # ('ice', OneHotEncoder(), ["Embarked"]),
         # ('burg', OneHotEncoder(handle_unknown="ignore"), ["Ticket"]),
         # ('white', OneHotEncoder(handle_unknown="ignore"), ["Cabin"]),
+        ('lifeboat', StandardScaler(), ["Fare"]),
+        # ('ice', StandardScaler(), ["Parch"]),
+        # ('burg', StandardScaler(), ["SibSp"]),
+        # ('white', StandardScaler(), ["Age"]),
         ('star', OneHotEncoder(handle_unknown="ignore"), ["Suffix"])
     ])
 
 X_train_preprocessed = preprocessor.fit_transform(X_train)
 X_test_preprocessed = preprocessor.transform(X_test)
 
+# -------------------------------------- LOGISTIC REGRESSION FIT ------------------------------------ #
 model.fit(X_train_preprocessed, y_train)
 
-y_pred = model.predict(X_test_preprocessed)
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy}")  # Accuracy: 0.6756756756756757
-# **************** After removing Embarked, Ticket, and Cabin Accuracy is 0.8108108108108109 **************** #
+lr_y_pred = model.predict(X_test_preprocessed)
 
-coefficients = model.coef_[0]
-for feature, coef in zip(features, coefficients):
-    print(f"Feature {feature}: {coef}")
+lr_accuracy = accuracy_score(y_test, lr_y_pred)
+print(f"Logistic Regression Accuracy: {lr_accuracy}")  # Accuracy: 0.6756756756756757
+# **************** After removing Embarked, Ticket, and Cabin Accuracy is 0.8108108108108109 **************** #
+# coefficients = model.coef_[0]
+# for feature, coef in zip(features, coefficients):
+#     print(f"Feature {feature}: {coef}")
 # print(data["Cabin"].unique())
 
+# -------------------------------------- DECISION TREE FIT ------------------------------------ #
+clf = DecisionTreeClassifier()
+clf.fit(X_train_preprocessed, y_train)
+clf_y_pred = clf.predict(X_test_preprocessed)
+clf_accuracy = accuracy_score(y_test, clf_y_pred)
+print(f"Decision Tree Accuracy: {clf_accuracy}")
+
+# -------------------------------------- RANDOM FOREST FIT ------------------------------------ #
+rf = RandomForestClassifier(n_estimators=1000)
+rf.fit(X_train_preprocessed, y_train)
+rf_y_pred = rf.predict(X_test_preprocessed)
+rf_accuracy = accuracy_score(y_test, rf_y_pred)
+print(f"Random Forest Accuracy: {rf_accuracy}")
+
 # plt.figure()
-# plt.scatter(base_data["Pclass"], base_data["Cabin"])
+# plt.scatter(base_data["Survived"], base_data["CabinSuffix"])
 
 # for column in data.columns:
 #     if column not in ["PassengerId", "Survived", "Name"]:
@@ -148,15 +172,24 @@ for feature, coef in zip(features, coefficients):
 # plt.show()
 
 test_data = pd.read_csv("Data/test.csv")
+median_fare = data["Fare"].median()
+test_data["Fare"] = test_data["Fare"].fillna(median_fare)
+
 test_data[["LastName", "SuffixFirstName"]] = test_data["Name"].str.split(",", expand=True)
 test_data[["Suffix", "FirstName"]] = test_data["SuffixFirstName"].str.split(".", n=1, expand=True)
 test_data.drop(columns=["Name", "SuffixFirstName"], inplace=True)
+missing_age_mask = test_data["Age"].isna()
+test_data.loc[missing_age_mask, "Age"] = test_data.loc[missing_age_mask, "Suffix"].map(median_age_by_suffix)
+print(test_data.describe())
 X_test = test_data[features]
 X_test_preprocessed = preprocessor.transform(X_test)
-y_pred = model.predict(X_test_preprocessed)
+
+# y_pred = model.predict(X_test_preprocessed)
+y_pred = rf.predict(X_test_preprocessed)
 test_data["Survived"] = y_pred
 predictions = test_data[["PassengerId", "Survived"]]
 print(predictions)
+predictions.to_csv("Data/submission.csv")
 # accuracy = accuracy_score(y_test, y_pred)
 # print(f"Accuracy: {accuracy}")  # Accuracy: 0.6756756756756757
 
